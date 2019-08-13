f7448ee5df54f7dbf8fec44a54c9229836b89726
Jeno <lishujie0571@163.com>
2019-08-13 21:01:44 +0800

    [finish] array01. Removes duplicates from a sorted array

--- PythonVersion/Algorithm/Easy/Array01.py
+++ PythonVersion/Algorithm/Easy/Array01.py
@@ -1,17 +1,20 @@
-from typing import List
+# 从排序数组中删除重复项
+# 思路：两个指针一前一后遍历，当low指针小于high指针时，low指针+1，并且low+1指向high（即把大值复制到当前低值位）
 
+from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums: return 0
-        k = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[k] = nums[i]
-                k += 1
-        return k
+        low=0
+        for high in range(1,len(nums)):
+            if nums[low]<nums[high]:
+                low+=1
+                nums[low]=nums[high]
+        return low+1
 
 
 
 
-
+obj=Solution()
+print(obj.removeDuplicates([1,1,2]))
---
+++ PythonVersion/Algorithm/Easy/Array02.py
@@ -0,0 +1,6 @@
+# 买卖股票的最佳时机 II
+# 思路：贪心 或 动态规划
+
+# 贪心
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
