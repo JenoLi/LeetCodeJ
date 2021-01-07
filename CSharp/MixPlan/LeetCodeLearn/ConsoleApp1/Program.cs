0be458ccc1531d3e7a52b0940b70db94f632cd58
Jeno <lishujie0571@163.com>
2021-01-07 23:23:40 +0800

    [ADD] 253：比较

--- CSharp/MixPlan/LeetCodeLearn/ConsoleApp1/Program.cs
+++ CSharp/MixPlan/LeetCodeLearn/ConsoleApp1/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace First
 {
@@ -10,23 +8,38 @@ namespace First
     {
         static void Main(string[] args)
         {
-            string a = "0123";
-            int b = a[2]-'0';
-            Console.Write(b);
-            Console.ReadKey();
-            // MinMeetingRooms(new int[][] { [2, 9],[1, 10] });
-
+            // string a = "0123";
+            // int b = a[2]-'0';//自动转换成十进制int类型，不 -‘0’ 会输出ascii码
+            // Console.WriteLine(b);
+            // Console.ReadKey();
+            // int[][] a = new int[2][2] {{2, 9}, {1, 10}};
+            int[][] a = {new int[]{2,9}, new int[]{1, 10}};
+            MinMeetingRooms(a);
         }
 
-        public int MinMeetingRooms(int[][] intervals)
+        public static int MinMeetingRooms(int[][] intervals)
         {
-            Array.Sort(intervals);
+            Array.Sort(intervals,new MyComparable());
             foreach (int[] i in intervals)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(i[0]+","+i[1]);
+                // foreach (int i1 in i)
+                // {
+                //     Console.WriteLine(i1);
+                // }
             }
             Console.ReadKey();
             return 1;
         }
+        public class MyComparable:IComparer<int[]>
+        {
+            public  int Compare(int[] x, int[] y)
+            {
+                return x[0] - y[0];//返回值大于0表示x>y,等于0表示x=y,小于0表示x<y。Array.Sort内部会根据这个返回值来判断x和y的大小关系，并把小的元素放在前面
+                //如果想降序怎么办，返回y[0]-x[0]即可
+            }
+
+        }
     }
+
 }
