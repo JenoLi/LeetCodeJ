d20ebd7f2c14617ae142b6853189da09eda85e7c
lishujie <lishujie@51talk.com>
2021-11-11 16:01:22 +0800

    [fix] N100整理写法

--- CSharp/MixPlan/LeetCodeLearn/Second2021/Tree/N100.cs
+++ CSharp/MixPlan/LeetCodeLearn/Second2021/Tree/N100.cs
@@ -8,15 +8,14 @@ namespace Second2021.Tree
         }
         public bool CompareTree(TreeNode _left,TreeNode _right)
         {
-            if (_left == null && _right != null)//一个空 一个不空
-                return false;
-            else if (_left != null && _right == null)//一个空 一个不空
-                return false;
-            else if (_left ==null && _right==null)//都空
-                return true;
-            else if (_left.val != _right.val)//都不空且不等
-                return false;
-            else return CompareTree(_left.left, _right.left) && CompareTree(_left.right, _right.right);//相等则继续
+            if (_left == null && _right == null) return true;//都为空
+            else if (_left != null && _right == null) return false;//一个为空
+            else if (_left == null && _right != null) return false;
+            else if (_left.val != _right.val) return false;//不为空但不等
+            else//不为空且相等，继续递归
+            {
+                return CompareTree(_left.left, _right.left) && CompareTree(_left.right, _right.right);
+            }
         }
     }
 }
