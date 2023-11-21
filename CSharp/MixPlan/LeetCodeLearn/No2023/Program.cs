29898c7345696bbb2f9f0caa45b288d6a60dcf2d
SENSETIME\lishujie <lishujie@softsugar.com>
2023-11-21 19:46:31 +0800

    [ADD]102 144

---
+++ CSharp/MixPlan/LeetCodeLearn/No2023/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace No2023
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+        
+
+    }
+    
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ CSharp/MixPlan/LeetCodeLearn/No2023/Tree/N144.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace No2023.Tree
+{
+    public class N144
+    {
+        //中序 右中左
+        public IList<int> InorderInteration(TreeNode root)
+        {
+            List<int> res = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            if(root!=null)
+                stack.Push(root);
+            while (stack.Count>0)
+            {
+                TreeNode temp = stack.Pop();
+                if (temp != null)
+                {
+                    if(temp.right!=null) stack.Push(temp.right);
+                    stack.Push(temp);
+                    stack.Push(null);
+                    if(temp.left!=null) stack.Push(temp.left);
+                }
+                else
+                {
+                    res.Add(stack.Pop().val);
+                }
+            }
+
+            return res;
+        }
+    }
+}
