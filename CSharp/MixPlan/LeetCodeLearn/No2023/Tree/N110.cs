a13fb34665deb4edbfde7eecdaecd4f3b4062b7e
SENSETIME\lishujie <lishujie@softsugar.com>
2023-11-24 19:52:50 +0800

    104 110

---
+++ CSharp/MixPlan/LeetCodeLearn/No2023/Tree/N110.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace No2023.Tree
+{
+    public class N110
+    {
+        public bool IsBalance(TreeNode node)
+        {
+            int depth = GetDepth(node);
+            return depth == -1 ? false : true;
+        }
+
+        public int GetDepth(TreeNode node)
+        {
+            if (node == null) return -1;
+            int left = GetDepth(node.left);
+            if (left == -1) return -1;
+            int right = GetDepth(node.right);
+            if (right == -1) return -1;
+
+            int res = 0;
+            if (Math.Abs(left - right) > 1)
+                res = -1;
+            else
+            {
+                res = 1 + Math.Max(left, right);
+            }
+
+            return res;
+        }
+    }
+}
